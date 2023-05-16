32e7590943bb7040a034580197c730da32a73c10
ryo ono <ryo.ono@gmail.com>
2023-05-16 20:54:08 +0900

    インデックスに一覧表表示作成まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(params.require(:post).permit(:content))
+    redirect_to new_post_path
   end
 end
