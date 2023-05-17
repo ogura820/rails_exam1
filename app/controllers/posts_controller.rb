cb3eed21aa3fbd7aa0fc79e80f9034ff712d00ee
ryo ono <ryo.ono@gmail.com>
2023-05-17 14:08:14 +0900

    編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy], except: [:confirm]
+  before_action :set_post, only: [:show, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -12,10 +12,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
       redirect_to posts_path, notice: "つぶやいちゃった"
     else
-      # 入力フォームを再描画します。
       render :new
     end
   end
@@ -23,9 +21,6 @@ class PostsController < ApplicationController
   def confirm
     @post = Post.new(post_params)
   end
-
-  
-
 
   def show
   end
@@ -36,6 +31,17 @@ class PostsController < ApplicationController
     redirect_to posts_path, notice: "つぶやきを削除しました！"
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
 
       private
 
